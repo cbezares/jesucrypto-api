db7b5ec4baac4ebc858e5f352110aae5b3440856
Caio Bezares <cbezares.info@gmail.com>
2018-01-30 01:45:08 -0300

    Updating clockwork

--- lib/clock.rb
+++ lib/clock.rb
@@ -4,30 +4,28 @@ require File.expand_path('../../config/environment', __FILE__)
 
 include Clockwork
 
-module Clockwork
-  handler do |job|
-    puts "[clockwork:handler] Running #{job}"
-  end
+handler do |job|
+  puts "[clockwork:handler] Running #{job}"
+end
 
-  every(1.day, '[clockwork:exchange:update_exchanges] Updating exchanges data.') do
-    ExchangesServices::Status.update_exchanges
-  end
+every(1.day, '[clockwork:exchange:update_exchanges] Updating exchanges data.') do
+  ExchangesServices::Status.update_exchanges
+end
 
-  every(2.minutes, '[clockwork:exchange:update_important] Updating important exchange prices.') do
-    exchanges = %w(BDA ORX XAP SXC CLB CMK)
-    ExchangesServices::Status.update_prices(exchanges)
-  end
+every(2.minutes, '[clockwork:exchange:update_important] Updating important exchange prices.') do
+  exchanges = %w(BDA ORX XAP SXC CLB CMK)
+  ExchangesServices::Status.update_prices(exchanges)
+end
 
-  # every(5.minutes, '[clockwork:exchange:update_others] Updating less important exchange prices.') do
-  #   exchanges = %w(BSP CBS STT BNC BTK)
-  #   ExchangesServices::Status.update_prices(exchanges)
-  # end
+# every(5.minutes, '[clockwork:exchange:update_others] Updating less important exchange prices.') do
+#   exchanges = %w(BSP CBS STT BNC BTK)
+#   ExchangesServices::Status.update_prices(exchanges)
+# end
 
-  every(1.hour, '[clockwork:exchange:update_exchanges] Updating miner fees.') do
-    ExchangesServices::Status.update_miner_fees
-  end
+every(1.hour, '[clockwork:exchange:update_miner_fees] Updating miner fees.') do
+  ExchangesServices::Status.update_miner_fees
+end
 
-  every(5.minutes, '[clockwork:exchange:update_arbitrages] Updating arbitrage opportunities.') do
-    ExchangesServices::Status.update_arbitrages
-  end
+every(2.minutes, '[clockwork:exchange:update_arbitrages] Updating arbitrage opportunities.') do
+  ExchangesServices::Status.update_arbitrages
 end
