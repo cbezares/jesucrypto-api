502af8896da89e8987286d99e1ca32efc21431d5
Caio Bezares <cbezares.info@gmail.com>
2018-01-30 01:59:18 -0300

    Updating clockwork

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,33 +1,32 @@
 require 'clockwork'
-require 'exchanges_services'
-require File.expand_path('../../config/boot',        __FILE__)
-require File.expand_path('../../config/environment', __FILE__)
+require './config/boot'
+require './config/environment'
 
-include Clockwork
-include ExchangesServices
+module Clockwork
 
-handler do |job|
-  puts "[clockwork:handler] Running #{job}"
-end
+  handler do |job|
+    puts "[clockwork:handler] Running #{job}"
+  end
 
-every(1.day, '[clockwork:exchange:update_exchanges] Updating exchanges data.') do
-  ExchangesServices::Status.update_exchanges
-end
+  every(1.day, '[clockwork:exchange:update_exchanges] Updating exchanges data.') do
+    ExchangesServices::Status.update_exchanges
+  end
 
-every(2.minutes, '[clockwork:exchange:update_important] Updating important exchange prices.') do
-  exchanges = %w(BDA ORX XAP SXC CLB CMK)
-  ExchangesServices::Status.update_prices(exchanges)
-end
+  every(2.minutes, '[clockwork:exchange:update_important] Updating important exchange prices.') do
+    exchanges = %w(BDA ORX XAP SXC CLB CMK)
+    ExchangesServices::Status.update_prices(exchanges)
+  end
 
-# every(5.minutes, '[clockwork:exchange:update_others] Updating less important exchange prices.') do
-#   exchanges = %w(BSP CBS STT BNC BTK)
-#   ExchangesServices::Status.update_prices(exchanges)
-# end
+  # every(5.minutes, '[clockwork:exchange:update_others] Updating less important exchange prices.') do
+  #   exchanges = %w(BSP CBS STT BNC BTK)
+  #   ExchangesServices::Status.update_prices(exchanges)
+  # end
 
-every(1.hour, '[clockwork:exchange:update_miner_fees] Updating miner fees.') do
-  ExchangesServices::Status.update_miner_fees
-end
+  every(1.hour, '[clockwork:exchange:update_miner_fees] Updating miner fees.') do
+    ExchangesServices::Status.update_miner_fees
+  end
 
-every(2.minutes, '[clockwork:exchange:update_arbitrages] Updating arbitrage opportunities.') do
-  ExchangesServices::Status.update_arbitrages
+  every(2.minutes, '[clockwork:exchange:update_arbitrages] Updating arbitrage opportunities.') do
+    ExchangesServices::Status.update_arbitrages
+  end
 end
